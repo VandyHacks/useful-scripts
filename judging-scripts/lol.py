b3d840cffe1a3c5821141f07ecdd6fb19c8fa4c4
Baibhav Vatsa <baibhavvatsa@gmail.com>
2020-10-04 10:46:02 -0500

    updates

--- judging-scripts/lol.py
+++ judging-scripts/lol.py
@@ -1,18 +1,19 @@
 import csv
 
 id = 0
+numprojects = 0
 projects = []
-with open("submissions.csv") as f:
+with open("submissions.csv", encoding="utf-8") as f:
   r = csv.reader(f)
   for row in r:
-    if id != 0 and row[0] != "Vandyhacks":
+    if id != 0 and row[1] != "https://vandyhacks-retro-edn.devpost.com/submissions/178913-vandyhacks":
       projects.append({
         'title': row[0],
         'url': row[1]
       })
+      numprojects += 1
     id += 1
-print(id)
-
+print(numprojects)
 judges = []
 jnames = ["Shalini Gupta", "Tim Eccleston", "Prof. Julie Johnson", "Prof. Catie Chang", "Eliott Fernando", "Michael Copeland", "Krish Munot", "Lucas Remedios", "Aleksandr Zakharov", "Arjun Sharma", "Irfaan", "Matt", "Dr. Molvig"]
 print(len(jnames))
@@ -26,7 +27,7 @@ for j in range(jn):
   judges[j] += f"{jnames[j]}: \n"
   for pid in pids:
     i = int(pid) - 1
-    judges[j] += f"- {projects[i]['url']}\n"
+    judges[j] += f"- [{i+1}]: {projects[i]['title']} {projects[i]['url']}\n"
   judges[j] += "\n"
 
 f2 = open("out.txt", "w")
