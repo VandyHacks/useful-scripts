ba48e73d3ec580b458c880331e1ee01d354facb5
Baibhav Vatsa <baibhavvatsa@gmail.com>
2020-10-04 10:22:00 -0500

    add names

--- judging-scripts/lol.py
+++ judging-scripts/lol.py
@@ -5,24 +5,29 @@ projects = []
 with open("submissions.csv") as f:
   r = csv.reader(f)
   for row in r:
-    if id != 0:
+    if id != 0 and row[0] != "Vandyhacks":
       projects.append({
         'title': row[0],
         'url': row[1]
       })
     id += 1
+print(id)
 
 res = 'y'
 judges = []
-jn = int(input("Enter number of judges: "))
+jnames = ["Shalini Gupta", "Tim Eccleston", "Prof. Julie Johnson", "Prof. Catie Chang", "Eliott Fernando", "Michael Copeland", "Krish Munot", "Lucas Remedios", "Aleksandr Zakharov", "Arjun Sharma", "Irfaan", "Matt", "Dr. Molvig"]
+print(len(jnames))
+jn = len(jnames)
+# jn = int(input("Enter number of judges: "))
 for j in range(jn):
   judges.append("")
 for j in range(jn):
   plist = input("Copy paste their project list ids: ")
   pids = plist.split(", ")
-  judges[j] += "Judge 1: \n"
+  judges[j] += f"{jnames[j]}: \n"
   for pid in pids:
-    i = int(pid)
+    i = int(pid) - 1
+    print(i)
     judges[j] += f"- {projects[i]['url']}\n"
   judges[j] += "\n"
 
