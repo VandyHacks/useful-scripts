48778878e1df49d6078ed660fc8ca389cfb4d1eb
Baibhav Vatsa <baibhavvatsa@gmail.com>
2020-10-04 11:05:41 -0500

    what am i doing

--- judging-scripts/tallying_scores.py
+++ judging-scripts/tallying_scores.py
@@ -1,11 +1,11 @@
 import csv
 from collections import OrderedDict
 
-project_num = 15
-t_w = 3
-c_w = 2
-top = 5
-p_w = 2
+project_num = 51
+t_w = 1
+c_w = 1
+top = 10
+p_w = 1
 u_w = 1
 i_w = 1
 scores = {}
@@ -13,12 +13,11 @@ idk = {}
 for project in range(project_num):
   scores[project + 1] = []
 
-# https://forms.gle/2xqQgfFYmA8ryH1U6 form
-with open('./responses.csv') as csv_file:
-  csv_reader = csv.reader(csv_file, delimiter=',')
+with open('responses.csv', encoding='utf-8') as csv_file:
+  csv_reader = csv.reader(csv_file)
   line_count = 0
   for row in csv_reader:
-    if line_count != 0:
+    if line_count != 0 and row[2] != '' and row[4] != '' and row[3] != '' and row[5] != '' and row[6] != '' and row[7] != '':
       table = int(row[2])
       technical = int(row[3])
       creativity = int(row[4])
@@ -28,19 +27,12 @@ with open('./responses.csv') as csv_file:
       final_score = (t_w * technical) + (u_w * utility) + (c_w * creativity) + (p_w * presentation) + (impression * i_w)
       scores[table] = scores[table] + [final_score]
     line_count += 1
-  for table in scores:
-    scores[table] = sum(scores[table])/len(scores[table])
-    if scores[table] in idk:
-      idk[scores[table]] = idk[scores[table]] + [table]
-    else:
-      idk[scores[table]] = [table]
-  i = 1
-  idk_o = OrderedDict(sorted(idk.items(), reverse=True))
-  for k, v in idk_o.items():
-    if i > top:
+  for tnum in range(1, len(scores) + 1):
+    scores[tnum] = sum(scores[tnum])/len(scores[tnum])
+  # print(scores)
+  lol = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1], reverse=True)}
+  for k, v in lol.items():
+    print(f"Project {k}: {v}")
+    top -= 1
+    if top <= 0:
       break
-    else:
-      print(f'score {k} was achieved by projects: {v}')
-    i += 1
-
-
